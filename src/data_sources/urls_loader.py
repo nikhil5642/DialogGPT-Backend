00cbc1e5447bb60cdab423ac5d850e0fd0200a98
Nikhil agrawal <nikhilagrawal5642@gmail.com>
2023-09-13 22:57:43 +0530

    removed a minor check

--- src/data_sources/urls_loader.py
+++ src/data_sources/urls_loader.py
@@ -51,7 +51,7 @@ def get_all_urls_mapping(base_url, max_depth=5):
                 for link in soup.find_all("a", href=True):
                     new_url = urljoin(base_url, link["href"])
                     parsed_url = urlparse(new_url)
-                    if parsed_url.netloc == base_netloc and not parsed_url.fragment and isValidUrl(new_url):
+                    if parsed_url.netloc == base_netloc and not parsed_url.fragment :
                         urls_to_visit.append((new_url, depth + 1))
 
                 # Store the text content associated with the current URL
