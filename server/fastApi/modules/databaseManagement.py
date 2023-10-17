78b24e74aaeb09026c63a08f6db9b33474ed1f42
Nikhil agrawal <nikhilagrawal5642@gmail.com>
2023-10-18 04:00:51 +0530

    leads collection added

--- server/fastApi/modules/databaseManagement.py
+++ server/fastApi/modules/databaseManagement.py
@@ -1,7 +1,7 @@
 from fastapi import HTTPException
 from DataBase.MongoDB import getChatBotConfigCollection, getChatHistoryCollection, getContentStoreCollection, getUsersCollection,getChatBotsCollection
 from src.BaseConstants import BASE_CHAT_BUBBLE_COLOR, BASE_CHAT_INITIAL_MESSAGE, BASE_CHATGPT_PROMPT, BASE_USER_MSG_COLOR
-from src.DataBaseConstants import BASE_PROMPT, CHAT_ID, COMPLETE, CONTENT_LIST, EMAIL_ID, FREE_PLAN, GPT_3_5_TURBO, HISTORY, INTERFACE, LAST_UPDATED, LIGHT, MESSAGE_CREDITS, MESSAGE_LIMIT,MESSAGE_USED, MODEL, MODEL_VERSION, PROMPT, SUBSCRIPTION_CANCELED, SUBSCRIPTION_PLAN, SUBSCRIPTION_STATUS, TEMPERATURE, TRAINED, USER_ID,CHATBOT_ID,CHATBOT_LIST,CONTENT_ID,CONTENT,CHATBOT_NAME,CHATBOT_STATUS,CREATED_ON,CHAR_COUNT,SUBSCRIPTION_ID,INITIAL_MESSAGE,QUICK_PROMPTS,THEME,PROFILE_PICTURE,USER_MSG_COLOR,DISPLAY_NAME,CHAT_ICON,CHAT_BUBBLE_COLOR
+from src.DataBaseConstants import BASE_PROMPT, CHAT_ID, COMPLETE, CONTENT_LIST, EMAIL_ID, FREE_PLAN, GPT_3_5_TURBO, HISTORY, INTERFACE, LAST_UPDATED, LIGHT, MESSAGE_CREDITS, MESSAGE_LIMIT,MESSAGE_USED, MODEL, MODEL_VERSION, PROMPT, SUBSCRIPTION_CANCELED, SUBSCRIPTION_PLAN, SUBSCRIPTION_STATUS, TEMPERATURE, TRAINED, USER_ID,CHATBOT_ID,CHATBOT_LIST,CONTENT_ID,CONTENT,CHATBOT_NAME,CHATBOT_STATUS,CREATED_ON,CHAR_COUNT,SUBSCRIPTION_ID,INITIAL_MESSAGE,QUICK_PROMPTS,THEME,PROFILE_PICTURE,USER_MSG_COLOR,DISPLAY_NAME,CHAT_ICON,CHAT_BUBBLE_COLOR,LEADS_COLLECTION,LEAD_EMAIL,LEAD_NAME,LEAD_PHONE
 from src.data_sources.utils import generateContentItem
 from src.emailSender.sendEmail import sendWelcomeEmail
 from src.logger.logger import GlobalLogger
@@ -41,7 +41,7 @@ def createChatBot(uid:str,chatbotname):
     bot_id_list = user.get(CHATBOT_LIST, [])
     max_allowed_bots= getChatBotLimitAsPerPlan(user.get(SUBSCRIPTION_PLAN, FREE_PLAN))
     if(len(bot_id_list)>=max_allowed_bots):
-        raise HTTPException(status_code=501, detail="You have reached the maximum limit of chatbots for your plan")
+        raise HTTPException(status_code=400, detail="You have reached the maximum limit of chatbots for your plan")
     bot_id_list.append({CHATBOT_ID:botID,CHATBOT_NAME:chatbotname,CHATBOT_STATUS:'untrained',LAST_UPDATED:datetime.now(),CREATED_ON:datetime.now()})
     getUsersCollection().update_one({USER_ID: uid}, {"$set": {CHATBOT_LIST: bot_id_list}})
     getChatBotsCollection().insert_one({USER_ID:uid,CHATBOT_ID:botID,CHATBOT_NAME:chatbotname,CHATBOT_STATUS:'untrained',LAST_UPDATED:datetime.now(),CREATED_ON:datetime.now()})
@@ -131,18 +131,45 @@ def storeContentList(list):
 def insertContentListInBotCollection(uid:str,botID:str,newContentList):
     getChatBotsCollection().update_one({USER_ID: uid, CHATBOT_ID: botID}, {"$set": {CONTENT_LIST: newContentList}})
 
+def getChatLeadCollection(botID:str):
+    document = getChatBotConfigCollection().find_one({CHATBOT_ID: botID})
+    if document:
+        leads_data = document.get(LEADS_COLLECTION, [])
+    else:
+        leads_data = []
+        
+    return leads_data   
+
+def setLeadsCollection(uid:str,botID:str,leadCollection:[str]):
+    if not getChatBotConfigCollection().find_one({USER_ID:uid,CHATBOT_ID:botID}):
+         getChatBotConfigCollection().insert_one({USER_ID:uid,CHATBOT_ID:botID,LEADS_COLLECTION:leadCollection})
+    else:
+        getChatBotConfigCollection().update_one({USER_ID: uid, CHATBOT_ID: botID}, {"$set": {LEADS_COLLECTION: leadCollection}})
+
+def storeLead(chatId,leadName,leadEmail,leadPhone):
+    existing_history = getChatHistoryCollection().find_one({CHAT_ID: chatId})
+    if existing_history:
+        getChatHistoryCollection().update_one(
+            {CHAT_ID: chatId},
+            {"$set": {LEAD_NAME: leadName,LEAD_EMAIL:leadEmail,LEAD_PHONE:leadPhone,LAST_UPDATED:datetime.now()}}
+        )
+    else:
+        getChatHistoryCollection().insert_one({LEAD_NAME: leadName,LEAD_EMAIL:leadEmail,LEAD_PHONE:leadPhone,LAST_UPDATED:datetime.now()})
+
 def getChatInterface(botID:str):
     document = getChatBotConfigCollection().find_one({CHATBOT_ID: botID})
     if document:
         interface_data = document.get(INTERFACE, {})
+        leads_data = document.get(LEADS_COLLECTION, [])
     else:
         interface_data = {}
+        leads_data = []
         
     if interface_data == {}:
-        return {INITIAL_MESSAGE:BASE_CHAT_INITIAL_MESSAGE,QUICK_PROMPTS:"",THEME:LIGHT,PROFILE_PICTURE:None,USER_MSG_COLOR:BASE_USER_MSG_COLOR,DISPLAY_NAME:"",CHAT_ICON:None,CHAT_BUBBLE_COLOR:BASE_CHAT_BUBBLE_COLOR}
+        return {INITIAL_MESSAGE:BASE_CHAT_INITIAL_MESSAGE,QUICK_PROMPTS:"",THEME:LIGHT,PROFILE_PICTURE:None,USER_MSG_COLOR:BASE_USER_MSG_COLOR,DISPLAY_NAME:"",CHAT_ICON:None,CHAT_BUBBLE_COLOR:BASE_CHAT_BUBBLE_COLOR,LEADS_COLLECTION:leads_data}
     else:
-        return {INITIAL_MESSAGE:interface_data[INITIAL_MESSAGE] or BASE_CHAT_INITIAL_MESSAGE,QUICK_PROMPTS:interface_data[QUICK_PROMPTS],THEME:interface_data[THEME],PROFILE_PICTURE:interface_data[PROFILE_PICTURE],USER_MSG_COLOR:interface_data[USER_MSG_COLOR],DISPLAY_NAME:interface_data[DISPLAY_NAME],CHAT_ICON:interface_data[CHAT_ICON],CHAT_BUBBLE_COLOR:interface_data[CHAT_BUBBLE_COLOR]}    
-        
+        return {INITIAL_MESSAGE:interface_data[INITIAL_MESSAGE] or BASE_CHAT_INITIAL_MESSAGE,QUICK_PROMPTS:interface_data[QUICK_PROMPTS],THEME:interface_data[THEME],PROFILE_PICTURE:interface_data[PROFILE_PICTURE],USER_MSG_COLOR:interface_data[USER_MSG_COLOR],DISPLAY_NAME:interface_data[DISPLAY_NAME],CHAT_ICON:interface_data[CHAT_ICON],CHAT_BUBBLE_COLOR:interface_data[CHAT_BUBBLE_COLOR],LEADS_COLLECTION:leads_data}    
+
 
 def updateChatInterface(uid:str,botID:str,initialMessage:str,quickPrompts:str,theme:str,profilePicture:str,userMsgColor:str,displayName:str,chatIcon:str,chatBubbleColor:str):
     interface_data={ INITIAL_MESSAGE:initialMessage,QUICK_PROMPTS:quickPrompts,THEME:theme,PROFILE_PICTURE:profilePicture,USER_MSG_COLOR:userMsgColor,DISPLAY_NAME:displayName,CHAT_ICON:chatIcon,CHAT_BUBBLE_COLOR:chatBubbleColor}
