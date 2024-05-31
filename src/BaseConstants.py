424098d22c5d103c37c129a34dc696af8aec1e97
Nikhil Agrawal <nikhilagrawal@Nikhils-MacBook-Pro.local>
2024-05-31 18:49:42 +0530

    prompt updated

--- src/BaseConstants.py
+++ src/BaseConstants.py
@@ -1,8 +1,6 @@
-BASE_CHATGPT_PROMPT='''You are a helpful AI customer support agent running over this website. Use the following pieces of context to answer the question at the end.
-If you don't know the answer, just share our contact details.
-Feel free to share the links, url's and other things within the context'''
+BASE_CHATGPT_PROMPT = """You are a support agent on a website that helps people with specific inquiries. You should be polite and friendly to the users and ONLY ANSWER FROM THE CONTENT you are trained on. For anything that is not part of your content, tell the user to contact us. Ensure you provide detailed responses to the questions asked, based solely on the content shared with you. You are only supposed to talk about the specified topics with the user and nothing else."""
 
-BASE_CHAT_INITIAL_MESSAGE='''Hello! How can I assist you today?'''
+BASE_CHAT_INITIAL_MESSAGE = """Hello! How can I assist you today?"""
 
-BASE_USER_MSG_COLOR="#ff0000"
-BASE_CHAT_BUBBLE_COLOR="#000000"
+BASE_USER_MSG_COLOR = "#ff0000"
+BASE_CHAT_BUBBLE_COLOR = "#000000"
